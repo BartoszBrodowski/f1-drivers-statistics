e8035a45836d81d8c978024972571059d41d0961
BartoszBrodowski <bartolini10058@gmail.com>
2023-08-20 15:39:39 +0200

    refactor: make code more concise

--- src/f1scraper.py
+++ src/f1scraper.py
@@ -10,71 +10,88 @@ session = requests.Session()
 CURRENT_YEAR = date.today().year
 
 
-def get_soup(website):
-    page = session.get(website)
+def get_soup(link):
+    page = session.get(link)
     page.raise_for_status()
     soup = BeautifulSoup(page.content, "lxml")
     return soup
+
+def get_filtered_text(text):
+    return text.get_text().replace("\n", " ").strip()
+
+def filter_row_no_limiters(row):
+    return row.find_all("td", class_=lambda value: value is None or "limiter" not in value.split())
+
+def get_link_by_season(season, link):
+    return get_soup(link.format(season))
+
+def get_table_rows_without_header(soup):
+    return soup.find_all("tr")[1:]
 
 
 class F1StatsScraper:
     # Drivers
 
     def get_drivers_by_season(self, season=CURRENT_YEAR):
-        drivers = []
-        website = f"https://www.formula1.com/en/results.html/{season}/drivers.html"
-        soup = get_soup(website)
-        for driver in soup.find_all("tr")[1:]:
-            nationality = driver.find(
-                "td", class_="dark semi-bold uppercase"
-            ).get_text()
-            # Transforming name to not contain a name tag
-            name_array_without_tag = (
-                driver.find("a").get_text().strip().splitlines()[:-1]
-            )
-            name_transformed = " ".join(name_array_without_tag)
-            drivers.append((name_transformed, nationality))
+        link = f"https://www.formula1.com/en/results.html/{season}/drivers.html"
+        soup = get_soup(link)
+        table_rows = get_table_rows_without_header(soup)
+
+        def get_name(driver):
+            name_parts = driver.find("a").get_text().strip().splitlines()[:-1]
+            return " ".join(name_parts)
+        
+        def get_nationality(driver):
+            return driver.find("td", class_="dark semi-bold uppercase").get_text()
+
+        drivers = [
+            (get_name(driver), get_nationality(driver))
+            for driver in table_rows
+        ]
+
         return drivers
 
     def get_drivers_by_range(self, start_season, end_season):
-        drivers = set()
-        for season in range(start_season, end_season + 1):
-            website = f"https://www.formula1.com/en/results.html/{season}/drivers.html"
-            soup = get_soup(website)
-            for driver in soup.find_all("tr")[1:]:
-                nationality = driver.find(
-                    "td", class_="dark semi-bold uppercase"
-                ).get_text()
-                # Transforming name to not contain a name tag
-                name_array_without_tag = (
-                    driver.find("a").get_text().strip().splitlines()[:-1]
-                )
-                name_transformed = " ".join(name_array_without_tag)
-                drivers.add((name_transformed, nationality))
+        def get_name(driver):
+            name_parts = driver.find("a").get_text().strip().splitlines()[:-1]
+            return " ".join(name_parts)
+
+        def get_nationality(driver):
+            return driver.find("td", class_="dark semi-bold uppercase").get_text()
+        
+        drivers = {
+            (get_name(driver), get_nationality(driver))
+            for season in range(start_season, end_season + 1)
+            for driver in get_table_rows_without_header(get_soup(f"https://www.formula1.com/en/results.html/{season}/drivers.html"))
+        }
+
         return drivers
 
     # Races
 
     def get_races_by_season(self, season=CURRENT_YEAR):
         races = []
-        website = f"https://www.formula1.com/en/results.html/{season}/races.html"
-        soup = get_soup(website)
-        for race in soup.find_all("tr")[1:]:
+        link = f"https://www.formula1.com/en/results.html/{season}/races.html"
+        soup = get_soup(link)
+        table_rows = get_table_rows_without_header(soup)
+        for race in table_rows:
             name = race.find("td", class_="dark bold").get_text().strip()
             races.append(name)
         return races
 
     def get_races_by_range(self, start_season, end_season):
-        races = {}
-        for season in range(start_season, end_season + 1):
-            races_by_season = []
-            website = f"https://www.formula1.com/en/results.html/{season}/races.html"
-            soup = get_soup(website)
-            for race in soup.find_all("tr")[1:]:
-                name = race.find("td", class_="dark bold").get_text().strip()
-                races_by_season.append(name)
-            races[season] = races_by_season
-        return races
+        def get_race_name(race):
+            return get_filtered_text(race.find("td", class_='dark bold'))
+        
+        link = "https://www.formula1.com/en/results.html/{}/races.html"
+
+        return {
+            season: [
+                get_race_name(race)
+                for race in get_table_rows_without_header(get_link_by_season(season, link))
+            ]
+            for season in range(start_season, end_season + 1)
+        }
 
     # Links
 
@@ -84,186 +101,140 @@ class F1StatsScraper:
             championship_links[
                 season
             ] = f"https://www.formula1.com/en/results.html/{season}/championship.html"
+
         return championship_links
 
     def get_race_link(self, season, race):
-        website = f"https://www.formula1.com/en/results.html/{season}/races.html"
-        soup = get_soup(website)
-        race_tag = soup.find(lambda tag: tag.name == "a" and race in tag.text)
-        if race_tag is not None:
-            link = race_tag.get("href")
-            return "https://www.formula1.com" + link
-        return None
+        link = f"https://www.formula1.com/en/results.html/{season}/races.html"
+        soup = get_soup(link)
+
+        def text_contains_whitespace_insensitive(element, target_text):
+            return element.name == 'a' and element.get_text().strip() == target_text.strip()
+
+        element = soup.find(lambda tag: text_contains_whitespace_insensitive(tag, race))
+        if element is None:
+            return None
+        
+        tag_link = element['href']
+        race_link = f'https://formula1.com{tag_link}'
+
+        return race_link
 
     def get_all_race_links_by_season(self, season):
-        grand_prix_links = {}
-        website = f"https://www.formula1.com/en/results.html/{season}/races.html"
-        soup = get_soup(website)
-        grand_prix_list = soup.select("td > a")
-        for grand_prix in grand_prix_list:
-            grand_prix_name = grand_prix.get_text().strip()
-            grand_prix_href = grand_prix.get("href")
-            link = "https://www.formula1.com" + grand_prix_href
-            grand_prix_links[grand_prix_name] = link
+        link = f"https://www.formula1.com/en/results.html/{season}/races.html"
+        soup = get_soup(link)
+        grand_prix_anchor_tags = soup.select("td > a")
+
+        grand_prix_links = {
+            get_filtered_text(tag): f"https://www.formula1.com{tag.get('href')}" for tag in grand_prix_anchor_tags
+        }
 
         return grand_prix_links
 
-    def get_race_links_every_season(self, race):
-        race_links = {}
-        for season in range(1950, CURRENT_YEAR + 1):
-            race_link = self.get_race_link(season, race)
-            if race_link is not None:
-                race_links[season] = race_link
+    def get_one_races_links_every_season(self, race):
+        race_links = {season: race_link for season in range(1950, CURRENT_YEAR + 1) if (race_link := self.get_race_link(season, race))}
 
         return race_links
 
     def get_driver_links_by_season(self, season):
-        websites = []
-        drivers = self.get_drivers_by_season(season)
-        for driver in drivers:
-            print(driver)
+        def get_driver_tag(driver):
             driver_name = unidecode(driver[0].replace(" ", "-").lower())
-            print(driver_name)
+            # Get the first part of the driver tag
             tag_1st_part = driver[0][:3].upper()
-            # Get second part of URL driver tag (e.g. LEWHAM01 for Lewis Hamilton)
+            # Get second part of URL tag (e.g. LEWHAM01 for Lewis Hamilton)
             tag_2nd_part = "".join(driver[0].replace("'", "").split()[1:])[:3].upper()
             # 3rd part of URL driver tag is always 01
             tag = tag_1st_part + tag_2nd_part + "01"
-            website = f"https://www.formula1.com/en/results.html/{season}/drivers/{tag}/{driver_name}.html"
-            websites.append(website)
 
-        return websites
+            return driver_name, tag
+
+        link = "https://www.formula1.com/en/results.html/{}/drivers/{}/{}.html" 
+        drivers = self.get_drivers_by_season(season)
+
+        links = [
+            link.format(season, tag, driver_name)
+            for driver in drivers 
+            for driver_name, tag in [get_driver_tag(driver)]
+        ]
+
+        return links
 
     # Race results
 
     def get_all_race_results_by_range(self, start_season, end_season):
+        def delete_driver_tag(name):
+            return name[:-4]
         seasons = {}
+
         for season in range(start_season, end_season + 1):
-            seasons[season] = {}
             race_links = self.get_all_race_links_by_season(season)
-
-            # Add race names
-            for race in race_links:
-                seasons[season][race] = []
-
-            # Add driver names, positions and points
-            for race, link in race_links.items():
-                soup = get_soup(link)
-                table_rows = soup.select("tr")[1:]
-                filtered_rows = [row for row in table_rows]
-
-                for row in filtered_rows:
-                    row_text = [
-                        elem.text.strip().replace("\n", " ")[:-4]
-                        if i == 3
-                        else elem.text.replace("\n", " ")
-                        for i, elem in enumerate(row.find_all(["td"]))
-                        if "limiter" not in elem.get("class")
+            seasons[season] = {
+                race: [
+                    [
+                        # Delete last 4 characters (tag) if the element is the drivers name
+                        delete_driver_tag(get_filtered_text(elem)) if i == 3 else get_filtered_text(elem)
+                        for i, elem in enumerate(filter_row_no_limiters(row))
                     ]
-                    seasons[season][race].append(row_text)
-
+                    for row in filtered_rows
+                ]
+                for race, link in race_links.items()
+                if (soup := get_soup(link))
+                and (filtered_rows := [row for row in get_table_rows_without_header(soup)])
+            }
         return seasons
 
     # Championship stats
 
     def get_drivers_championship_stats(self, season=CURRENT_YEAR):
-        drivers_stats = []
-        website = f"https://www.formula1.com/en/results.html/{season}/drivers.html"
-        soup = get_soup(website)
+        link = f"https://www.formula1.com/en/results.html/{season}/drivers.html"
+        soup = get_soup(link)
 
-        driver_table_rows = soup.find_all("tr")[1:]
+        driver_table_rows = get_table_rows_without_header(soup)
 
         drivers_stats = [
-            [
-                stat.get_text().replace("\n", " ").strip()
-                for stat in row.find_all(
-                    "td",
-                    class_=lambda value: value is None
-                    or "limiter" not in value.split(),
-                )
-            ]
+            [get_filtered_text(stat) for stat in filter_row_no_limiters(row)]
             for row in driver_table_rows
         ]
 
         return drivers_stats
 
     def get_drivers_championship_stats_by_range(self, start_season, end_season):
-        drivers_stats = {}
-
-        for season in range(start_season, end_season + 1):
-            website = f"https://www.formula1.com/en/results.html/{season}/drivers.html"
-            soup = get_soup(website)
-
-            driver_table_rows = soup.find_all("tr")[1:]
-
-            driver_stats = [
-                [
-                    stat.get_text().replace("\n", " ").strip()
-                    for stat in row.find_all(
-                        "td",
-                        class_=lambda value: value is None
-                        or "limiter" not in value.split(),
-                    )
-                ]
-                for row in driver_table_rows
+        link = "https://www.formula1.com/en/results.html/{}/drivers.html"
+        return {
+            season: [
+                [get_filtered_text(stat) for stat in filter_row_no_limiters(row)]
+                for row in get_table_rows_without_header(get_link_by_season(season, link))
             ]
-
-            drivers_stats[season] = driver_stats
-
-        return drivers_stats
+            for season in range(start_season, end_season + 1)
+        }
 
     def get_constructors_championship_stats(self, season=CURRENT_YEAR):
-        website = f"https://www.formula1.com/en/results.html/{season}/team.html"
-        soup = get_soup(website)
-
-        driver_table_rows = soup.find_all("tr")[1:]
+        link = f"https://www.formula1.com/en/results.html/{season}/team.html"
+        soup = get_soup(link)
+        driver_table_rows = get_table_rows_without_header(soup)
 
         driver_stats = [
-            [
-                stat.get_text().replace("\n", " ").strip()
-                for stat in row.find_all(
-                    "td",
-                    class_=lambda value: value is None
-                    or "limiter" not in value.split(),
-                )
-            ]
+            [get_filtered_text(stat) for stat in filter_row_no_limiters(row)]
             for row in driver_table_rows
         ]
 
         return driver_stats
 
     def get_constructors_championship_stats_by_range(self, start_season, end_season):
-        constructor_stats = {}
+        link = "https://www.formula1.com/en/results.html/{}/team.html"
 
-        for season in range(start_season, end_season + 1):
-            website = f"https://www.formula1.com/en/results.html/{season}/team.html"
-            soup = get_soup(website)
-
-            driver_table_rows = soup.find_all("tr")[1:]
-
-            driver_stats = [
-                [
-                    stat.get_text().replace("\n", " ").strip()
-                    for stat in row.find_all(
-                        "td",
-                        class_=lambda value: value is None
-                        or "limiter" not in value.split(),
-                    )
-                ]
-                for row in driver_table_rows
-            ]
-
-            constructor_stats[season] = driver_stats
-
-        return constructor_stats
+        return {
+            season: [
+                [get_filtered_text(stat) for stat in filter_row_no_limiters(row)] for row in get_table_rows_without_header(get_link_by_season(season, link))]
+            for season in range(start_season, end_season + 1)
+        }
 
     # Champions
 
     def get_drivers_world_champion(self, season=CURRENT_YEAR - 1):
-        website = (
-            f"https://en.wikipedia.org/wiki/{season}_Formula_One_World_Championship"
-        )
-        soup = get_soup(website)
+        link = "https://en.wikipedia.org/wiki/{}_Formula_One_World_Championship"
+        link = get_link_by_season(season, link)
+        soup = get_soup(link)
         champion = (
             soup.find(class_="motorsport-season-nav-subheader")
             .select("a")[1]
@@ -273,10 +244,10 @@ class F1StatsScraper:
         return champion
 
     def get_constructors_world_champion(self, season=CURRENT_YEAR - 1):
-        website = (
+        link = (
             f"https://en.wikipedia.org/wiki/{season}_Formula_One_World_Championship"
         )
-        soup = get_soup(website)
+        soup = get_soup(link)
         champion = (
             soup.find(class_="motorsport-season-nav-subheader")
             .select("a")[3]
@@ -285,11 +256,10 @@ class F1StatsScraper:
 
         return champion
 
-
 def main():
     scraper = F1StatsScraper()
 
-    test = scraper.get_drivers_by_range(1950, 2023)
+    test = scraper.get_drivers_by_range(2016,2023)
     print(test)
 
     session.close()
